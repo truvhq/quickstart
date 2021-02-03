69c22262e548bdc0144686963fab568cc5829927
Rey Riel <rey@flamebaud.ca>
2021-02-02 20:22:28 -0500

    adding error handling

--- golang/citadel.go
+++ golang/citadel.go
@@ -21,7 +21,7 @@ type AccessTokenResponse struct {
 	AccessTokens []string `json:"access_tokens"`
 }
 
-func getRequest(endpoint string, method string, body []byte) *http.Request {
+func getRequest(endpoint string, method string, body []byte) (*http.Request, error) {
 	apiUrl := os.Getenv("API_URL")
 	clientId := os.Getenv("API_CLIENT_ID")
 	accessKey := os.Getenv("API_SECRET")
@@ -30,84 +30,98 @@ func getRequest(endpoint string, method string, body []byte) *http.Request {
 	request.Header.Set("Content-Type", "application/json")
 	request.Header.Set("X-Access-Client-Id", clientId)
 	request.Header.Set("X-Access-Secret", accessKey)
-	return request
+	return request, nil
 }
 
-func getBridgeToken() string {
-	request := getRequest("bridge-tokens/", "POST", nil)
+func getBridgeToken() (string, error) {
+	request, err := getRequest("bridge-tokens/", "POST", nil)
+	if err != nil {
+		return "", err
+	}
 	client := &http.Client{}
 	response, err := client.Do(request)
+
 	if err != nil {
-		fmt.Printf("The HTTP request failed with error %s\n", err)
-	} else {
-		data, _ := ioutil.ReadAll(response.Body)
-		return (string(data))
+		return "", err
 	}
-	return ""
+		data, _ := ioutil.ReadAll(response.Body)
+		return (string(data)), nil
 }
 
-func getAccessToken(public_token string) string {
+func getAccessToken(public_token string) (string, error) {
 	publicTokens := PublicTokenRequest{PublicTokens: []string{public_token}}
 	jsonPublicTokens, _ := json.Marshal(publicTokens)
 	accessTokens := AccessTokenResponse{}
-	request := getRequest("access-tokens/", "POST", jsonPublicTokens)
+	request, err := getRequest("access-tokens/", "POST", jsonPublicTokens)
+	if err != nil {
+		return "", err
+	}
 	client := &http.Client{}
 	res, err := client.Do(request)
 	defer res.Body.Close()
 
 	if err != nil {
-		fmt.Printf("The HTTP request failed with error %s\n", err)
+		return "", err
 	}
 	err = json.NewDecoder(res.Body).Decode(&accessTokens)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
-	return accessTokens.AccessTokens[0]
+	return accessTokens.AccessTokens[0], nil
 }
 
-func getEmploymentInfoByToken(access_token string) string {
+func getEmploymentInfoByToken(access_token string) (string, error) {
 	accessToken := AccessTokenRequest{AccessToken: access_token}
 	jsonAccessToken, _ := json.Marshal(accessToken)
-	request := getRequest("verifications/employments", "POST", jsonAccessToken)
+	request, err := getRequest("verifications/employments", "POST", jsonAccessToken)
+	if err != nil {
+		return "", err
+	}
 	client := &http.Client{}
 	res, err := client.Do(request)
 	defer res.Body.Close()
 
 	if err != nil {
-		fmt.Printf("The HTTP request failed with error %s\n", err)
+		return "", err
 	}
 	data, _ := ioutil.ReadAll(res.Body)
-	return string(data)
+	return string(data), nil
 }
 
-func getIncomeInfoByToken(access_token string) string {
+func getIncomeInfoByToken(access_token string) (string, error) {
 	accessToken := AccessTokenRequest{AccessToken: access_token}
 	jsonAccessToken, _ := json.Marshal(accessToken)
-	request := getRequest("verifications/incomes", "POST", jsonAccessToken)
+	request, err := getRequest("verifications/incomes", "POST", jsonAccessToken)
+	if err != nil {
+		return "", err
+	}
 	client := &http.Client{}
 	res, err := client.Do(request)
 	defer res.Body.Close()
 
 	if err != nil {
-		fmt.Printf("The HTTP request failed with error %s\n", err)
+		return "", err
 	}
 	data, _ := ioutil.ReadAll(res.Body)
-	return string(data)
+	return string(data), nil
 }
 
-func getEmployeeDirectoryByToken(access_token string) string {
+func getEmployeeDirectoryByToken(access_token string) (string, error) {
 	accessToken := AccessTokenRequest{AccessToken: access_token}
 	jsonAccessToken, _ := json.Marshal(accessToken)
-	request := getRequest("administrators/directories", "POST", jsonAccessToken)
+	request, err := getRequest("administrators/directories", "POST", jsonAccessToken)
+	if(err != nil) {
+		return "", err
+	}
 	client := &http.Client{}
 	res, err := client.Do(request)
 	defer res.Body.Close()
 
 	if err != nil {
-		fmt.Printf("The HTTP request failed with error %s\n", err)
+		return "", err
 	}
 	data, _ := ioutil.ReadAll(res.Body)
-	return string(data)
+	return string(data), nil
 }
 
 type PayrollReportRequest struct {
@@ -120,34 +134,40 @@ type PayrollReportResponse struct {
 	PayrollReportId string `json:"payroll_report_id"`
 }
 
-func requestPayrollReport(access_token, start_date, end_date string) PayrollReportResponse {
+func requestPayrollReport(access_token, start_date, end_date string) (*PayrollReportResponse, error) {
 	reportRequest := PayrollReportRequest{AccessToken: access_token, StartDate: start_date, EndDate: end_date}
 	jsonReportRequest, _ := json.Marshal(reportRequest)
 	payrollReport := PayrollReportResponse{}
-	request := getRequest("administrators/payrolls", "POST", jsonReportRequest)
+	request, err := getRequest("administrators/payrolls", "POST", jsonReportRequest)
+	if err != nil {
+		return nil, err
+	}
 	client := &http.Client{}
 	res, err := client.Do(request)
 	defer res.Body.Close()
 
 	if err != nil {
-		fmt.Printf("The HTTP request failed with error %s\n", err)
+		return nil, err
 	}
 	err = json.NewDecoder(res.Body).Decode(&payrollReport)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	return payrollReport
+	return &payrollReport, nil
 }
 
-func getPayrollById(reportId string) string {
-	request := getRequest(fmt.Sprintf("administrators/payrolls/%s", reportId), "GET", nil)
+func getPayrollById(reportId string) (string, error) {
+	request, err := getRequest(fmt.Sprintf("administrators/payrolls/%s", reportId), "GET", nil)
+	if err != nil {
+		return "", err
+	}
 	client := &http.Client{}
 	res, err := client.Do(request)
 	defer res.Body.Close()
 
 	if err != nil {
-		fmt.Printf("The HTTP request failed with error %s\n", err)
+		return "", err
 	}
 	data, _ := ioutil.ReadAll(res.Body)
-	return string(data)
+	return string(data), nil
 }
--- golang/main.go
+++ golang/main.go
@@ -26,36 +26,98 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 }
 
 func bridgeToken(w http.ResponseWriter, r *http.Request) {
-	bridgeData := getBridgeToken()
-	fmt.Fprintf(w, bridgeData)
+	bridgeData, err := getBridgeToken()
+	if err != nil {
+		fmt.Println("Error in bridgeToken\n", err)
+		fmt.Fprintf(w, `{ "success": false }`)
+	} else {
+		fmt.Fprintf(w, bridgeData)
+	}
 }
 
 func verifications(w http.ResponseWriter, r *http.Request) {
 	productType := os.Getenv("API_PRODUCT_TYPE")
 	splitPath := strings.Split(r.URL.Path, "/")
 	token := splitPath[2]
-	accessToken := getAccessToken(token)
+	accessToken, err := getAccessToken(token)
+	if err != nil {
+		fmt.Println("Error getting access token", err)	
+		fmt.Fprintf(w, `{ "success": false }`)
+		return
+	}
 	verificationResponse := ""
 	if productType == "employment" {
-		verificationResponse = getEmploymentInfoByToken(accessToken)
+		verificationResponse, err = getEmploymentInfoByToken(accessToken)
 	} else {
-		verificationResponse = getIncomeInfoByToken(accessToken)
+		verificationResponse, err = getIncomeInfoByToken(accessToken)
 	}
-	fmt.Fprintf(w, verificationResponse)
+	if err != nil {
+		fmt.Println("Error getting verification", err)	
+		fmt.Fprintf(w, `{ "success": false }`)
+	} else {
+		fmt.Fprintf(w, verificationResponse)
+	}
 }
 
 func adminData(w http.ResponseWriter, r *http.Request) {
 	splitPath := strings.Split(r.URL.Path, "/")
 	token := splitPath[2]
-	accessToken := getAccessToken(token)
-	directory := getEmployeeDirectoryByToken(accessToken)
-	report := requestPayrollReport(accessToken, "2020-01-01", "2020-10-31")
+	accessToken, err := getAccessToken(token)
+	if err != nil {
+		fmt.Println("Error getting access token", err)	
+		fmt.Fprintf(w, `{ "success": false }`)
+		return
+	}
+	directory, err := getEmployeeDirectoryByToken(accessToken)
+	if err != nil {
+		fmt.Println("Error getting Employee Directory", err)	
+		fmt.Fprintf(w, `{ "success": false }`)
+		return
+	}
+	report, err := requestPayrollReport(accessToken, "2020-01-01", "2020-10-31")
+	if err != nil {
+		fmt.Println("Error requesting payroll report", err)	
+		fmt.Fprintf(w, `{ "success": false }`)
+		return
+	}
 	reportId := report.PayrollReportId
-	payroll := getPayrollById(reportId)
+	payroll, err := getPayrollById(reportId)
+	if err != nil {
+		fmt.Println("Error getting payroll by id", err)	
+		fmt.Fprintf(w, `{ "success": false }`)
+		return
+	}
 
 	data := fmt.Sprintf(`{ "directory": %s, "payroll": %s }`, directory, payroll)
 
 	fmt.Fprintf(w, data)
+}
+
+func checkEnv() {
+	apiUrl := os.Getenv("API_URL")
+	if apiUrl == "" {
+		fmt.Println("No API_URL provided")
+		os.Exit(1)
+	}
+	clientId := os.Getenv("API_CLIENT_ID")
+	if clientId == "" {
+		fmt.Println("No API_CLIENT_ID provided")
+		os.Exit(1)
+	}
+	accessKey := os.Getenv("API_SECRET")
+	if accessKey == "" {
+		fmt.Println("No API_SECRET provided")
+		os.Exit(1)
+	}
+	productType := os.Getenv("API_PRODUCT_TYPE")
+	if productType == "" {
+		fmt.Println("No API_PRODUCT_TYPE provided")
+		os.Exit(1)
+	}
+	if productType != "employment" && productType != "income" && productType != "admin" {
+		fmt.Println("API_PRODUCT_TYPE must be one of employment, income or admin")
+		os.Exit(1)
+	}
 }
 
 func handleRequests() {
@@ -64,17 +126,20 @@ func handleRequests() {
 	http.HandleFunc("/getVerifications/", verifications)
 	http.HandleFunc("/getAdminData/", adminData)
 
-	fmt.Println(strings.Repeat("=", 40), "ENVIRONMENT", strings.Repeat("=", 40))
-	fmt.Println(fmt.Sprintf("API_CLIENT_ID: %s", os.Getenv("API_CLIENT_ID")))
-	fmt.Println(fmt.Sprintf("API_SECRET: %s", os.Getenv("API_SECRET")))
-	fmt.Println(fmt.Sprintf("API_URL: %s", os.Getenv("API_URL")))
-	fmt.Println(fmt.Sprintf("API_PRODUCT_TYPE: %s", os.Getenv("API_PRODUCT_TYPE")))
-	fmt.Println(strings.Repeat("=", 94))
 	fmt.Println("listening on port 5000")
 
 	log.Fatal(http.ListenAndServe(":5000", nil))
 }
 
 func main() {
+	checkEnv()
+
+	fmt.Println(strings.Repeat("=", 40), "ENVIRONMENT", strings.Repeat("=", 40))
+	fmt.Println(fmt.Sprintf("API_CLIENT_ID: %s", os.Getenv("API_CLIENT_ID")))
+	fmt.Println(fmt.Sprintf("API_SECRET: %s", os.Getenv("API_SECRET")))
+	fmt.Println(fmt.Sprintf("API_URL: %s", os.Getenv("API_URL")))
+	fmt.Println(fmt.Sprintf("API_PRODUCT_TYPE: %s", os.Getenv("API_PRODUCT_TYPE")))
+	fmt.Println(strings.Repeat("=", 94))
+
 	handleRequests()
 }
