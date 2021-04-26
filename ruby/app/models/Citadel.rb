4bae0092d9edfe9247f50d61c3cd520393f497c5
Rey Riel <rey@flamebaud.ca>
2021-04-25 23:18:33 -0400

    adding logs to ruby

--- ruby/app/models/Citadel.rb
+++ ruby/app/models/Citadel.rb
@@ -8,48 +8,60 @@ class Citadel
 
   def self.getBridgeToken()
     # https://docs.citadelid.com/ruby#bridge-tokens_create
+    puts "CITADEL: Requesting bridge token from https://prod.citadelid.com/v1/bridge-tokens"
     body = { "product_type" => Citadel.product_type, "client_name" => "Citadel Quickstart", "tracking_info" => "1337" }.to_json
     return sendRequest('bridge-tokens/', body, "POST")
   end
 
   def self.getAccessToken(public_token)
     # https://docs.citadelid.com/?ruby#exchange-token-flow
+    puts "CITADEL: Exchanging a public_token for an access_token from https://prod.citadelid.com/v1/access-tokens"
+    puts "CITADEL: Public Token - #{public_token}"
     body = { "public_tokens" => [public_token] }.to_json
     return sendRequest('access-tokens/', body, "POST")["access_tokens"][0]
   end
 
   def self.getEmploymentInfoByToken(access_token)
     # https://docs.citadelid.com/?ruby#employment-verification
+    puts "CITADEL: Requesting employment verification data using an access_token from https://prod.citadelid.com/v1/verifications/employments"
+    puts "CITADEL: Access Token - #{access_token}"
     body = { "access_token" => access_token }.to_json
     sendRequest('verifications/employments/', body, "POST")
   end
 
   def self.getIncomeInfoByToken(access_token)
     # https://docs.citadelid.com/?ruby#income-verification
+    puts "CITADEL: Requesting income verification data using an access_token from https://prod.citadelid.com/v1/verifications/incomes"
+    puts "CITADEL: Access Token - #{access_token}"
     body = { "access_token" => access_token }.to_json
     sendRequest('verifications/incomes/', body, "POST")
   end
 
   def self.getEmployeeDirectoryByToken(access_token)
     # * https://docs.citadelid.com/?ruby#employee-directory
+    puts "CITADEL: Requesting employee directory data using an access_token from https://prod.citadelid.com/v1/administrators/directories"
+    puts "CITADEL: Access Token - #{access_token}"
     body = { "access_token" => access_token }.to_json
     sendRequest("administrators/directories/", body, "POST")
   end
 
   def self.requestPayrollReport(access_token, start_date, end_date)
     # https://docs.citadelid.com/?ruby#create-payroll-report
+    puts "CITADEL: Requesting a payroll report be created using an access_token from https://prod.citadelid.com/v1/administrators/payrolls"
+    puts "CITADEL: Access Token - #{access_token}"
     body = { "access_token" => access_token, "start_date" => start_date, "end_date" => end_date }.to_json
     sendRequest("administrators/payrolls/", body, "POST")
   end
 
   def self.getPayrollById(report_id)
     # https://docs.citadelid.com/?ruby#retrieve-payroll-report
+    puts "CITADEL: Requesting a payroll report using a report_id from https://prod.citadelid.com/v1/administrators/payrolls/{report_id}"
+    puts "CITADEL: Report ID - #{report_id}"
     sendRequest("administrators/payrolls/#{report_id}", nil, "GET")
   end
 
   def self.sendRequest(endpoint, body, method)
     uri = URI("https://prod.citadelid.com/v1/#{endpoint}")
-    puts "accessing #{endpoint}".inspect
     if method == "POST"
       req = Net::HTTP::Post.new uri
     else
