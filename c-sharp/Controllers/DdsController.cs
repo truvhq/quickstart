d3877439c13bf06da595c0840a5e6ff0e9d36f69
Rey Riel <rey@flamebaud.ca>
2021-06-10 10:24:04 -0400

    adding dds to csharp

---
+++ c-sharp/Controllers/DdsController.cs
@@ -0,0 +1,25 @@
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace c_sharp.Controllers
+{
+  [ApiController]
+  [Route("getDdsData")]
+  public class DdsController : ControllerBase
+  {
+
+    private Citadel _citadel = new Citadel();
+
+    [Route("{token}")]
+    [HttpGet]
+    public async Task<string> Get(string token)
+    {
+      var accessTokenResponse = await _citadel.GetAccessToken(token);
+      var parsedResponse = JsonDocument.Parse(accessTokenResponse);
+      var accessToken = parsedResponse.RootElement.GetProperty("access_token").GetString();
+
+      return await _citadel.GetDdsByToken(accessToken);
+    }
+  }
+}
--- c-sharp/Controllers/RootController.cs
+++ c-sharp/Controllers/RootController.cs
@@ -24,6 +24,9 @@ namespace c_sharp.Controllers
     public ContentResult Get()
     {
       var productType = Environment.GetEnvironmentVariable("API_PRODUCT_TYPE");
+      if (productType == "deposit_switch") {
+        productType = "dds";
+      }
       var fileContent = System.IO.File.ReadAllText($"../html/{productType}.html");
       fileContent = fileContent.Replace("{{ server_url }}", $"http://{Request.Host.Value}/");
       return base.Content(fileContent, "text/html");
