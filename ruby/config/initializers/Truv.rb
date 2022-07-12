2d8a4c4889c5e3a18035a7a673f61db65e1b1304
Dmitry Gerasimenko <kiddima@gmail.com>
2022-07-12 18:55:28 +0300

    fix: check env variables for ruby

--- ruby/config/initializers/Truv.rb
+++ ruby/config/initializers/Truv.rb
@@ -1,4 +1,16 @@
 Rails.configuration.to_prepare do
+  %w[
+    API_CLIENT_ID
+    API_SECRET
+    API_PRODUCT_TYPE
+  ].each do |env_var|
+    if !ENV.has_key?(env_var) || ENV[env_var].blank?
+      raise <<~EOL
+      Please provide #{env_var}
+      EOL
+    end
+  end
+
   Truv.client_id = ENV.fetch('API_CLIENT_ID')
   Truv.client_secret = ENV.fetch('API_SECRET')
   Truv.product_type = ENV.fetch('API_PRODUCT_TYPE')
