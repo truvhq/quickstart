93851582bc859968478a310d95425b3b23d4b771
kidig <dmitry.gerasimenko@truv.com>
2023-02-09 23:05:32 +0200

    use new API endpoints

--- c-sharp/Controllers/BridgeTokenController.cs
+++ c-sharp/Controllers/BridgeTokenController.cs
@@ -1,23 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace c_sharp.Controllers
 {
-  [ApiController]
-  [Route("getBridgeToken")]
-  public class BridgeTokenController : ControllerBase
-  {
-
-    private Truv _truv = new Truv();
-
-    [HttpGet]
-    public async Task<string> Get()
+    [ApiController]
+    [Route("getBridgeToken")]
+    public class BridgeTokenController : ControllerBase
     {
-      return await _truv.GetBridgeToken();
+
+        private Truv _truv = new Truv();
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            var userId = await _truv.CreateUser();
+            return await _truv.CreateUserBridgeToken(userId);
+        }
     }
-  }
 }
--- c-sharp/Controllers/DepositSwitchController.cs
+++ c-sharp/Controllers/DepositSwitchController.cs
@@ -1,25 +1,19 @@
-using System.Text.Json;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace c_sharp.Controllers
 {
-  [ApiController]
-  [Route("getDepositSwitchData")]
-  public class DepositSwitchController : ControllerBase
-  {
-
-    private Truv _truv = new Truv();
-
-    [Route("{token}")]
-    [HttpGet]
-    public async Task<string> Get(string token)
+    [ApiController]
+    [Route("getDepositSwitchData")]
+    public class DepositSwitchController : ControllerBase
     {
-      var accessTokenResponse = await _truv.GetAccessToken(token);
-      var parsedResponse = JsonDocument.Parse(accessTokenResponse);
-      var accessToken = parsedResponse.RootElement.GetProperty("access_token").GetString();
+        private Truv _truv = new Truv();
 
-      return await _truv.GetDepositSwitchByToken(accessToken);
+        [Route("{token}")]
+        [HttpGet]
+        public async Task<string> Get(string token)
+        {
+            var accessToken = await _truv.GetAccessToken(token);
+            return await _truv.GetDepositSwitchByToken(accessToken);
+        }
     }
-  }
 }
