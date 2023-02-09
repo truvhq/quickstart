3c649393a940bd0494a76ff74c7129a7cfd6b6c2
kidig <dmitry.gerasimenko@truv.com>
2023-02-09 23:05:13 +0200

    Update C# quickstart
    Migrate to .NET 6 (.NET 5 is outdated)

--- c-sharp/Program.cs
+++ c-sharp/Program.cs
@@ -1,20 +1,12 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+var builder = WebApplication.CreateBuilder(args);
 
-namespace c_sharp
-{
-  public class Program
-  {
-    public static void Main(string[] args)
-    {
-      CreateHostBuilder(args).Build().Run();
-    }
+// Add services to the container.
+builder.Services.AddControllers();
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-              webBuilder.UseStartup<Startup>();
-            });
-  }
-}
+var app = builder.Build();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
